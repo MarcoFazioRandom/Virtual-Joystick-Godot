e7397772b7e5b4ce6b5a6634ddf670c1a032655f
Marco F <51234379+MarcoFazioRandom@users.noreply.github.com>
2019-06-05 19:01:34 +0200

    Update Joystick.gd

--- Joystick/Joystick.gd
+++ Joystick/Joystick.gd
@@ -41,12 +41,9 @@ onready var _circle := $Background/Circle
 onready var _original_color : Color = _circle.modulate
 
 func _gui_input(event: InputEvent) -> void:
-	if (ui_mode == UI_Mode.MOVING or ui_mode == UI_Mode.FOLLOWING) and \
-			(event is InputEventScreenTouch and event.pressed):
-		
+	if (ui_mode == UI_Mode.MOVING or ui_mode == UI_Mode.FOLLOWING) and (event is InputEventScreenTouch and event.pressed):
 		var new_pos = event.position - _background.rect_size/2
 		_clamp_joystick_position(new_pos)
-
 		_circle.modulate = _pressed_color
 		is_working = true
 
@@ -60,15 +57,12 @@ func _on_Background_gui_input(event: InputEvent) -> void:
 	
 	if event is InputEventScreenDrag:
 		var vector : Vector2 = event.position - _background.rect_size/2
-		
 		# Control if the joystick is inside the deadzone
 		if vector.length() < dead_zone:
 			_set_circle_center_position(_background.rect_size/2)
 			output = Vector2.ZERO
 			return
-		
 		_update_output(vector)
-		
 		if ui_mode == UI_Mode.FOLLOWING:
 			_following(vector)
 
@@ -83,21 +77,12 @@ func _set_circle_center_position(new_position : Vector2) -> void:
 
 func _update_output(vector: Vector2) -> void:
 	vector = vector.clamped(clamp_zone)
-	
 	if directions > 0:
 		vector = _directional_vector(vector, directions, deg2rad(simmetry))
-	
 	_set_circle_center_position(vector + _background.rect_size/2)
-	
-	if vector_mode == Vector_Mode.NORMALIZED:
-		output = vector.normalized()
-	
-	elif vector_mode == Vector_Mode.REAL:
-		var vector_range = clamp_zone - dead_zone
-		var length = vector.length() - dead_zone
-		length = clamp(length, 0, vector_range)
-		var proportion = length / vector_range
-		output = vector.normalized() * proportion
+	output = vector.normalized()
+	if vector_mode == Vector_Mode.REAL:
+		output *= (vector.length() - dead_zone) / (clamp_zone - dead_zone)
 
 func _following(vector: Vector2) -> void:
 	if vector.length() > clamp_zone:
@@ -113,8 +98,7 @@ func _directional_vector(vector: Vector2, n_directions: int, simmetry := PI/2) -
 		angle = angle + 1 if angle >= 0 else angle - 1
 	angle *= PI / n_directions
 	angle -= simmetry
-	vector = Vector2(cos(angle), sin(angle)) * vector.length()
-	return vector
+	return Vector2(cos(angle), sin(angle)) * vector.length()
 
 func _clamp_joystick_position(new_position: Vector2):
 	new_position.x = clamp(new_position.x, -_background.rect_size.x / 2, rect_size.x - _background.rect_size.x / 2)
