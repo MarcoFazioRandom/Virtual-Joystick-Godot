656d9ec966ff88f171232bc6fb93a11ff5a1018f
Marco F <51234379+MarcoFazioRandom@users.noreply.github.com>
2019-06-21 19:57:10 +0200

    Updated files

--- Test/Player.gd
+++ Test/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 class_name Player
 
-export var speed := 300
+export var speed : float = 300
 
 onready var joystick_move := $UI/JoystickMove
 onready var joystick_look := $UI/JoystickLook
@@ -14,5 +14,5 @@ func _move(delta: float) -> void:
 	if joystick_move and joystick_move.is_working:
 		move_and_slide(joystick_move.output * speed)
 	
-	if joystick_look and joystick_look.is_working and joystick_look.output.length_squared() > 0:
+	if joystick_look and joystick_look.is_working:
 		rotation = joystick_look.output.angle()
