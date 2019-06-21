176dcfdde4961a4ff4003e97822f5a426e00053f
Marco F <51234379+MarcoFazioRandom@users.noreply.github.com>
2019-06-21 19:56:25 +0200

    Add files via upload

--- Joystick/Joystick.gd
+++ Joystick/Joystick.gd
@@ -2,24 +2,24 @@ extends Control
 
 class_name Joystick
 
-# STATIC: The joystick doesn't move.
-# MOVING: Every time the joystick area is pressed, the joystick position is set on the touched position.
-# FOLLOWING: If the finger moves outside the joystick background, the joystick follows it.
-enum UI_Mode {STATIC, MOVING, FOLLOWING}
-
-# REAL: return a vector with a lenght beetween 0 (deadzone) and 1; useful for implementing different velocity or acceleration.
-# NORMALIZED: return a normalized vector.
-enum Vector_Mode {REAL, NORMALIZED}
-
 # If the joystick is receiving inputs.
 var is_working := false
 
 # The joystick output.
 var output := Vector2.ZERO
 
-export(UI_Mode) var ui_mode := UI_Mode.STATIC
+# STATIC: The joystick doesn't move.
+# MOVING: Every time the joystick area is pressed, the joystick position is set on the touched position.
+# FOLLOWING: If the finger moves outside the joystick background, the joystick follows it.
+enum Joystick_mode {STATIC, MOVING, FOLLOWING}
 
-export(Vector_Mode) var vector_mode := Vector_Mode.REAL
+export(Joystick_mode) var joystick_mode := Joystick_mode.STATIC
+
+# REAL: return a vector with a lenght beetween 0 (deadzone) and 1; useful for implementing different velocity or acceleration.
+# NORMALIZED: return a normalized vector.
+enum Vector_mode {REAL, NORMALIZED}
+
+export(Vector_mode) var vector_mode := Vector_mode.REAL
 
 # The color of the button when the joystick is in use.
 export(Color) var _pressed_color := Color.gray
@@ -28,79 +28,79 @@ export(Color) var _pressed_color := Color.gray
 export(int) var directions := 0
 
 # It changes the angle of simmetry of the directions.
-export(int) var simmetry := 90
+export(int) var simmetry_angle := 90
 
-# When the joystick value is less than the deadzone, the output is zero.
-export(int, 64) var dead_zone := 20
+#If the handle is inside this range, in proportion to the background size, the output is zero.
+export(float, 0, 0.5) var dead_zone := 0.2;
 
-# The max distance the button can reach from the center.
-export var clamp_zone := 128
+#The max distance the handle can reach, in proportion to the background size.
+export(float, 0.5, 2) var clamp_zone := 1;
 
 onready var _background := $Background
-onready var _circle := $Background/Circle
-onready var _original_color : Color = _circle.modulate
+onready var _handle := $Background/Handle
+onready var _original_color : Color = _handle.modulate
+onready var _original_position : Vector2 = _background.rect_position
 
 func _gui_input(event: InputEvent) -> void:
-	if (ui_mode == UI_Mode.MOVING or ui_mode == UI_Mode.FOLLOWING) and (event is InputEventScreenTouch and event.pressed):
-		var new_pos = event.position - _background.rect_size/2
-		_clamp_joystick_position(new_pos)
-		_circle.modulate = _pressed_color
-		is_working = true
+	if event is InputEventScreenTouch and (joystick_mode == Joystick_mode.MOVING or joystick_mode == Joystick_mode.FOLLOWING):
+		if event.is_pressed():
+			var new_pos = event.position - _background.rect_size / 2
+			_background.rect_position = new_pos
+		else:
+			_background.rect_position = _original_position
 
 func _on_Background_gui_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
-		is_working = event.pressed
-		if is_working:
-			_circle.modulate = _pressed_color
+		if event.pressed:
+			_handle.modulate = _pressed_color
 		else:
-			_reset_joystick()
+			is_working = false
+			output = Vector2.ZERO
+			_set_handle_center_position(_background.rect_size / 2)
+			_handle.modulate = _original_color
 	
 	if event is InputEventScreenDrag:
-		var vector : Vector2 = event.position - _background.rect_size/2
-		# Control if the joystick is inside the deadzone
-		if vector.length() < dead_zone:
-			_set_circle_center_position(_background.rect_size/2)
+		var vector : Vector2 = event.position - _background.rect_size / 2
+		var dead_size = dead_zone * _background.rect_size.x / 2
+		if vector.length() < dead_size:
+			is_working = false
 			output = Vector2.ZERO
+			_set_handle_center_position(_background.rect_size / 2)
 			return
 		_update_output(vector)
-		if ui_mode == UI_Mode.FOLLOWING:
+		is_working = true
+		if joystick_mode == Joystick_mode.FOLLOWING:
 			_following(vector)
 
-func _reset_joystick() -> void:
-	is_working = false
-	output = Vector2.ZERO
-	_set_circle_center_position(_background.rect_size/2)
-	_circle.modulate = _original_color
-
-func _set_circle_center_position(new_position : Vector2) -> void:
-	_circle.rect_position = new_position - (_circle.rect_size / 2)
+func _set_handle_center_position(new_position : Vector2) -> void:
+	_handle.rect_position = new_position - _handle.rect_size / 2
 
 func _update_output(vector: Vector2) -> void:
-	vector = vector.clamped(clamp_zone)
+	var dead_size = dead_zone * _background.rect_size.x / 2
+	var clamp_size = clamp_zone * _background.rect_size.x / 2
+	vector = vector.clamped(clamp_size)
 	if directions > 0:
-		vector = _directional_vector(vector, directions, deg2rad(simmetry))
-	_set_circle_center_position(vector + _background.rect_size/2)
+		vector = _directional_vector(vector, directions, deg2rad(simmetry_angle))
 	output = vector.normalized()
-	if vector_mode == Vector_Mode.REAL:
-		output *= (vector.length() - dead_zone) / (clamp_zone - dead_zone)
+	if vector_mode == Vector_mode.REAL and vector.length() < clamp_size:
+		output *= (vector.length() - dead_size) / (clamp_size - dead_size)
+	_set_handle_center_position(output * clamp_size + _background.rect_size / 2)
 
 func _following(vector: Vector2) -> void:
-	if vector.length() > clamp_zone:
-		var radius = vector.normalized() * clamp_zone
+	var clamp_size = clamp_zone * _background.rect_size.x / 2
+	if vector.length() > clamp_size:
+		var radius = vector.normalized() * clamp_size
 		var delta = vector - radius
 		var new_pos = _background.rect_position + delta
-		_clamp_joystick_position(new_pos)
+		new_pos.x = clamp(new_pos.x, -_background.rect_size.x / 2, rect_size.x - _background.rect_size.x / 2)
+		new_pos.y = clamp(new_pos.y, -_background.rect_size.y / 2, rect_size.y - _background.rect_size.y / 2)
+		_background.rect_position = new_pos
 
-func _directional_vector(vector: Vector2, n_directions: int, simmetry := PI/2) -> Vector2:
-	var angle := (vector.angle() + simmetry) / (PI / n_directions)
+func _directional_vector(vector: Vector2, n_directions: int, simmetry_angle := PI/2) -> Vector2:
+	var angle := (vector.angle() + simmetry_angle) / (PI / n_directions)
 	angle = floor(angle) if angle >= 0 else ceil(angle)
 	if abs(angle) as int % 2 == 1:
 		angle = angle + 1 if angle >= 0 else angle - 1
 	angle *= PI / n_directions
-	angle -= simmetry
+	angle -= simmetry_angle
 	return Vector2(cos(angle), sin(angle)) * vector.length()
-
-func _clamp_joystick_position(new_position: Vector2):
-	new_position.x = clamp(new_position.x, -_background.rect_size.x / 2, rect_size.x - _background.rect_size.x / 2)
-	new_position.y = clamp(new_position.y,- _background.rect_size.y / 2, rect_size.y - _background.rect_size.y / 2)
-	_background.rect_position = new_position
