c1d95481afc839cf244b21245fa94e01ab0bbf96
Marco F <51234379+MarcoFazioRandom@users.noreply.github.com>
2021-01-21 16:58:55 +0100

    Add files via upload
    Fixed the warnings that appeared in the editor.
    Fixed the Joystick to start working as soon as it's been touched (on InputEventScreenTouch), previously it worked only when the touch was dragged (on InputEventScreenDrag) causing a bit of lag.
    Fixed the test scene to follow the official Godot's Docs 2d Movement guide.

--- Test/Player.gd
+++ Test/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 class_name Player
 
-export var speed : float = 300
+export var speed : float = 200
 
 export (NodePath) var joystickLeftPath
 onready var joystickLeft : Joystick = get_node(joystickLeftPath)
@@ -10,12 +10,9 @@ onready var joystickLeft : Joystick = get_node(joystickLeftPath)
 export (NodePath) var joystickRightPath
 onready var joystickRight : Joystick = get_node(joystickRightPath)
 
-func _physics_process(delta: float) -> void:
-	_move(delta)
-
-func _move(delta: float) -> void:
+func _physics_process(_delta):
 	if joystickLeft and joystickLeft.is_working:
-		move_and_slide(joystickLeft.output * speed)
+		var _velocity = move_and_slide(joystickLeft.output * speed)
 	
 	if joystickRight and joystickRight.is_working:
 		rotation = joystickRight.output.angle()
