2af76056907acb0aa12cbf0b067a735493c5d6ed
Marco F <51234379+MarcoFazioRandom@users.noreply.github.com>
2021-11-20 11:26:41 +0100

    Add files via upload

---
+++ Virtual Joystick/test/player.gd
@@ -0,0 +1,24 @@
+extends Sprite
+
+export var speed : float = 200
+
+export (NodePath) var joystickLeftPath
+onready var joystickLeft : VirtualJoystick = get_node(joystickLeftPath)
+
+export (NodePath) var joystickRightPath
+onready var joystickRight : VirtualJoystick = get_node(joystickRightPath)
+
+func _process(delta: float) -> void:
+	# Movement using the joystick output:
+#	if joystickLeft and joystickLeft.is_pressed():
+#		position += joystickLeft.get_output() * speed * delta
+	
+	# Movement using Input functions:
+	var move := Vector2.ZERO
+	move.x = Input.get_axis("ui_left", "ui_right")
+	move.y = Input.get_axis("ui_up", "ui_down")
+	position += move * speed * delta
+	
+	# Rotation:
+	if joystickRight and joystickRight.is_pressed():
+		rotation = joystickRight.get_output().angle()
